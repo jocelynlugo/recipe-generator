246253e153ffe4cee58a9c44f025804fab1f71f1
jocelynlugo <jocelynblugo@gmail.com>
2023-12-01 12:26:20 -0500

    added api to JavaScript

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
-  let recipeElement = document.querySelector("#recipe");
-
+function displayRecipe(response) {
   new Typewriter("#recipe", {
-    strings: "Recipe is here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "oaf35ec85ab9436b73053b0e51t48cf9";
+  let prompt = `Generate a quick and easy recipe about ${instructionsInput.value}`;
+  let context = `You are a expert at cooking and love to provide easy, quick meals. Your mission is to provide quick and easy recipes in basic HTML. User information is ${instructionsInput.value}.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
